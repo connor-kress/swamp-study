c3c835781da8cb74a5f55a308a56ed76dcfc8ab3
Connor Kress <ckress@ufl.edu>
2025-03-09 16:06:24 -0400

    Database registration and testing endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,34 @@
 import fastify from "fastify"
+import fastifyPostgres from "fastify-postgres"
 
 const server = fastify();
 
+const user = "...";
+const password = "...";
+const database = "swampstudy";
+const host = "localhost";
+const port = "5432";
+
+server.register(fastifyPostgres, {
+  connectionString: `postgres://${user}:${password}@${host}:${port}/${database}`,
+});
+
+server.get("/now", async (_, reply) => {
+  const client = await server.pg.connect();
+
+  try {
+    const { rows } = await client.query("SELECT NOW()");
+    return rows;
+  } catch (error) {
+    reply.code(500);
+    return { error: "Database error occurred" };
+  } finally {
+    client.release();
+  }
+});
+
 server.get("/ping", async (request, reply) => {
-  return "pong\n";
+  return "pong";
 });
 
 server.listen({ port: 3000 }, (err, address) => {
