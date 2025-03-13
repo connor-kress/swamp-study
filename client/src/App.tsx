cef2e5355375d46c40d5e3d8edb4bc4edad39477
gabeschreiber <gabriel.schreib@gmail.com>
2025-03-13 02:08:39 -0400

    began working on login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -23,7 +23,27 @@ function Home() {
 
 // Login Page (Placeholder)
 function Login() {
-  return <div></div>;
+  return (
+  <div style={{ textAlign: 'center', marginTop: '100px' }}>
+    <h1>Login</h1>
+    <p>Enter your UFL email and password to login</p>
+    <form style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <label style={{ textAlign: 'center' }}>
+        <input type="text" name="email" placeholder='Enter your UFL email...' />
+      </label>
+      <br />
+      <label style={{ textAlign: 'center' }}>
+        <input type="password" name="password" placeholder='Enter your password' />
+      </label>
+      <br />
+      <Link to="/signup" style={{ color: 'black' }}>New here? Sign up here!</Link>
+      <br />
+      <button type="submit" style={{ backgroundColor: '#C2D5C8', color: 'black', padding: '10px 20px', borderRadius: '10px', cursor: 'pointer', boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.3)' }}>
+        Join
+      </button>
+    </form>
+  </div>
+);
 };
 
 // Signup Page (Placeholder)
