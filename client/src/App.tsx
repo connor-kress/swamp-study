03ed6e596b7ad00026de93c2d395961fb88ff83c
Connor Kress <ckress@ufl.edu>
2025-03-20 19:16:55 -0400

    Auth screen formatting fixes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,20 @@ import { useState } from 'react';
 import viewIcon from './assets/view.png';
 import hideIcon from './assets/hide.png';
 
+// The SwampStudy logo text
+function SwampStudy() {
+  return (
+    <span>
+      <span style={{color: '#0021A5', fontWeight: 'bold'}}>Swamp</span>
+      <span style={{color: '#FA4616', fontWeight: 'bold'}}>Study</span>
+    </span>
+  );
+}
 
 function Home() {
   return (
     <div style={{ textAlign: 'center', marginTop: '100px' }}>
-      <h1>Welcome to <span style={{color: '#0021A5', fontWeight: 'bold'}}>Swamp</span><span style={{color: '#FA4616', fontWeight: 'bold'}}>Study</span></h1>
+      <h1>Welcome to <SwampStudy /></h1>
       <p>Your ultimate study companion for academic success</p>
       <div style={{ marginTop: '30px' }}>
         <Link to="/login">
@@ -15,7 +24,7 @@ function Home() {
             Login
           </button>
         </Link>
-        <Link to="/signup">
+        <Link to="/register">
           <button style={{ margin: '0 10px', padding: '10px 20px' }}>
             Sign Up
           </button>
@@ -23,57 +32,58 @@ function Home() {
       </div>
     </div>
   );
-};
+}
 
-// Login Page
-function Login() {
+function LoginScreen() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [passwordVisible, setPasswordVisible] = useState(false);
 
-  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+  function handleLogin(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     const data = { 
       email: (e.target as HTMLFormElement).email.value,
       password: (e.target as HTMLFormElement).password.value
-      };
+    };
     console.log(data);
 
     setEmail(data.email);
     setPassword(data.password);
 
-    console.log(`Email: ${email}, Password: ${password}`); //FIXME email and password are captured, send to backend (later)
-  };
+    console.log(`Email: ${email}, Password: ${password}`);
+    // TODO: Integrate with backend
+  }
 
-  const togglePasswordVisibility = () => {
-    setPasswordVisible(!passwordVisible);
-  };
 
   return (
   <div style={{ textAlign: 'center', marginTop: '100px' }}>
     <h1>Login</h1>
-    <p>Enter your UFL email and password to login</p>
-    <form style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} onSubmit={handleLogin}>
+    <p>Enter your UF email and password to login</p>
+    <form
+      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+      onSubmit={handleLogin}
+    >
       <label style={{ textAlign: 'center' }}>
-        <input 
-          type="text" 
-          id="email" 
-          name="email" 
-          placeholder='Enter your UFL email'
+        <input
+          type="text"
+          id="email"
+          name="email"
+          placeholder="UF email"
           style= {{ alignItems: 'center' }}
         />
       </label>
       <br />
       <label style={{ textAlign: 'center', paddingLeft: '20px' }}>
-        <input 
-          type={passwordVisible ? "text" : "password"} 
-          id="password" name="password" 
-          placeholder='Enter your password'
+        <input
+          type={passwordVisible ? "text" : "password"}
+          id="password"
+          name="password"
+          placeholder="password"
           style= {{ alignItems: 'center' }}
         />
       <img 
         src={passwordVisible ? viewIcon : hideIcon} 
-        onClick={togglePasswordVisibility} 
+        onClick={() => setPasswordVisible(prev => !prev)}
         alt={passwordVisible ? "Hide Password" : "View Password"} 
         style={{ 
           cursor: 'pointer', 
@@ -86,7 +96,9 @@ function Login() {
       />
       </label>
       <br />
-      <Link to="/signup" style={{ color: 'black' }}>New here? Sign up here!</Link>
+      <Link to="/register" style={{ color: 'black' }}>
+        New here? Sign up here!
+      </Link>
       <br />
       <button type="submit" style={{ backgroundColor: '#C2D5C8', color: 'black', padding: '10px 20px', borderRadius: '10px', cursor: 'pointer', boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.3)' }}>
         Log In
@@ -94,10 +106,9 @@ function Login() {
     </form>
   </div>
 );
-};
+}
 
-// Signup Page
-function Signup () {
+function RegisterScreen() {
   const [name, setName] = useState('');
   const [gradYear, setGradYear] = useState(0);
   const [email, setEmail] = useState('');
@@ -105,7 +116,7 @@ function Signup () {
   const [passwordVisible, setPasswordVisible] = useState(false);
 
 
-  const handleSignUp = (e: React.FormEvent<HTMLFormElement>) => {
+  function handleRegister(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     const data = {
       firstName: (e.target as HTMLFormElement).firstName.value,
@@ -116,67 +127,72 @@ function Signup () {
       };
     console.log(data);
 
-    setName(data.firstName + ' ' + data.lastName);
+    setName(`${data.firstName} ${data.lastName}`);
     setGradYear(data.gradYear);
     setEmail(data.email);
     setPassword(data.password);
 
-    console.log(`Name: ${name}, Grad Year: ${gradYear}, Email: ${email}, Password: ${password}`); //FIXME name, grad year, email, and password are captured, send to backend (later)
-  };
-
-  const togglePasswordVisibility = () => {
-    setPasswordVisible(!passwordVisible);
-  };
+    console.log(`Name: ${name}, Grad Year: ${gradYear
+                }, Email: ${email}, Password: ${password}`);
+    // TODO: send data to backend
+  }
 
   return (
     <>
     <div style={{ textAlign: 'center', marginTop: '100px' }}>
     <h1>Welcome to our Swamp!</h1>
-    <p>Find your classmates, friends, and new study buddies by joining us at <span style={{color: '#0021A5', fontWeight: 'bold'}}>Swamp</span><span style={{color: '#FA4616', fontWeight: 'bold'}}>Study</span></p>
+    <p>
+      Find your classmates, friends, and new study buddies by joining
+      us at <SwampStudy />
+    </p>
     <p>Fill out the below fields to get started!</p>
-    <form style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} onSubmit={handleSignUp}>
+    <form
+      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+      onSubmit={handleRegister}
+    >
       <label>
-        <input 
-          type="text" 
-          id="firstName" 
-          name="firstName" 
-          placeholder='First Name' 
+        <input
+          type="text"
+          id="firstName"
+          name="firstName"
+          placeholder="first name"
         />
       </label>
       <label>
-        <input 
-          type="text" 
-          id="lastName" 
-          name="lastName" 
-          placeholder='Last Name' 
+        <input
+          type="text"
+          id="lastName"
+          name="lastName"
+          placeholder="last name"
         />
       </label>
       <label>
-        <input 
-          type="number" 
-          id="gradYear" 
-          name="gradYear" 
-          placeholder='Graduation Year' 
+        <input
+          type="number"
+          id="gradYear"
+          name="gradYear"
+          placeholder="grad year"
         />
       </label>
       <label style={{ textAlign: 'center' }}>
         <input 
-          type="text" 
-          id="email" 
-          name="email" 
-          placeholder='UFL Email'
+          type="text"
+          id="email"
+          name="email"
+          placeholder="UF email"
         />
       </label>
       <label style={{ textAlign: 'center', paddingLeft: '20px' }}>
-        <input 
-            type={passwordVisible ? "text" : "password"} 
-            id="password" name="password" 
-            placeholder='Create a password'
+        <input
+            type={passwordVisible ? "text" : "password"}
+            id="password"
+            name="password"
+            placeholder="password"
             style= {{ alignItems: 'center' }}
           />
         <img 
           src={passwordVisible ? viewIcon : hideIcon} 
-          onClick={togglePasswordVisibility} 
+          onClick={() => setPasswordVisible(prev => !prev)}
           alt={passwordVisible ? "Hide Password" : "View Password"} 
           style={{ 
             cursor: 'pointer', 
@@ -189,7 +205,9 @@ function Signup () {
         />
       </label>
       <br />
-      <Link to="/login" style={{ color: 'black' }}>Already have an account? Login here!</Link>
+      <Link to="/login" style={{ color: 'black' }}>
+        Already have an account? Login here!
+      </Link>
       <br />
       <button type="submit" style={{ backgroundColor: '#C2D5C8', color: 'black', padding: '10px 20px', borderRadius: '10px', cursor: 'pointer', boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.3)' }}>
         Join
@@ -198,15 +216,15 @@ function Signup () {
   </div>
   </>
 );
-};
+}
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<LoginScreen />} />
+        <Route path="/register" element={<RegisterScreen />} />
       </Routes>
     </Router>
   );
