d35b3cc413d11d9241421e16d2301cd2ffa57d00
Connor Kress <ckress@ufl.edu>
2025-03-22 18:22:54 -0400

    Created docker image for server and docker compose with postgres

--- server/src/index.ts
+++ server/src/index.ts
@@ -27,7 +27,7 @@ server.get("/api/ping", async (_request, _reply) => {
   return "pong";
 });
 
-server.listen({ port: 3000 }, (err, address) => {
+server.listen({ port: 3000, host: "0.0.0.0" }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
