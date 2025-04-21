62c85c6f6d4a5df940a0591fd149ea163a5619e4
Connor Kress <ckress@ufl.edu>
2025-04-20 22:15:19 -0400

    memoized `useAuthFetch()`

--- client/src/hooks/useAuthFetch.ts
+++ client/src/hooks/useAuthFetch.ts
@@ -1,42 +1,46 @@
+import { useCallback } from "react";
 import { useNavigate } from "react-router";
 
 export function useAuthFetch() {
   const navigate = useNavigate();
 
-  async function authFetch(
-    input: RequestInfo,
-    init?: RequestInit,
-    redirectOnFail: boolean = true
-  ): Promise<Response> {
-    let response = await fetch(input, {
-      ...init,
-      credentials: "include",
-    });
-
-    if (response.status === 401) {
-      console.warn("Access token expired, attempting refresh...");
-      const refreshResponse = await fetch("/api/auth/refresh", {
-        method: "POST",
-        credentials: "include",
-      });
-
-      if (!refreshResponse.ok) {
-        if (redirectOnFail && location.pathname !== "/login") {
-          navigate("/login");
-        }
-        const data = await refreshResponse.json();
-        throw new Error(
-          `Unable to refresh token: ${data?.error || "Unknown error"}`
-        );
-      }
-
-      response = await fetch(input, {
+  const authFetch = useCallback(
+    async function (
+      input: RequestInfo,
+      init?: RequestInit,
+      redirectOnFail: boolean = true
+    ): Promise<Response> {
+      let response = await fetch(input, {
         ...init,
         credentials: "include",
       });
-    }
-    return response;
-  }
+
+      if (response.status === 401) {
+        console.warn("Access token expired, attempting refresh...");
+        const refreshResponse = await fetch("/api/auth/refresh", {
+          method: "POST",
+          credentials: "include",
+        });
+
+        if (!refreshResponse.ok) {
+          if (redirectOnFail && location.pathname !== "/login") {
+            navigate("/login");
+          }
+          const data = await refreshResponse.json();
+          throw new Error(
+            `Unable to refresh token: ${data?.error || "Unknown error"}`
+          );
+        }
+
+        response = await fetch(input, {
+          ...init,
+          credentials: "include",
+        });
+      }
+      return response;
+    },
+    [navigate]
+  );
 
   return authFetch;
 }
