d87fcfde05df569fc013b5e95f235704d458ed5d
Connor Kress <ckress@ufl.edu>
2025-04-20 22:23:40 -0400

    improved <TermDropdown /> with typing

--- client/src/components/TermDropdown.tsx
+++ client/src/components/TermDropdown.tsx
@@ -1,13 +1,24 @@
 import React from "react";
+import { CourseTerm } from "../types";
+
+export const courseTermData: {
+  value: string;
+  term: CourseTerm;
+  year: number;
+}[] = [
+  { value: "Spring 2025", term: "spring", year: 2025 },
+  { value: "Summer A 2025", term: "summer-a", year: 2025 },
+  { value: "Summer B 2025", term: "summer-b", year: 2025 },
+  { value: "Summer C 2025", term: "summer-c", year: 2025 },
+  { value: "Fall 2025", term: "fall", year: 2025 },
+];
 
 interface TermDropdownProps {
-    id: string;
-    name: string;
-    value: string;
-    onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  id: string;
+  name: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
 }
-
-
 
 export default function TermDropdown({
     id,
@@ -15,27 +26,19 @@ export default function TermDropdown({
     value,
     onChange,
 }: TermDropdownProps) {
-    const terms = [
-        { value: '', label: 'Select a term' },
-        { value: 'Spring 2025', label: 'Spring 2025' },
-        { value: 'Summer 2025', label: 'Summer 2025' },
-        { value: 'Fall 2025', label: 'Fall 2025' }
-    ];
-    const baseDropdownStyles = 'w-full px-3 py-2 border rounded-md';
-    
-    return (
-        <select
-            id={id}
-            name={name}
-            value={value}
-            onChange={onChange}
-            className={baseDropdownStyles}
-        >
-              {terms.map((term) => (
-              <option key={term.value} value={term.value}>
-                {term.label}
-              </option>
-              ))}
-        </select>
-    );
+  return (
+    <select
+        id={id}
+        name={name}
+        value={value}
+        onChange={onChange}
+        className= "w-full px-3 py-2 border rounded-md"
+    >
+      {courseTermData.map((term) => (
+        <option key={term.value} value={term.value}>
+          {term.value}
+        </option>
+      ))}
+    </select>
+  );
 }
